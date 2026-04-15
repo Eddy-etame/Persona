2750587ba627d2554f1027fd4d60910abafc23c6
Angoula Onambele Germain Raphael <farenogif05@gmail.com>
2026-04-15 17:09:46 +0200

    fix: allow render host in vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,7 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  preview: {
+    allowedHosts: ['persona-m4i2.onrender.com', '.onrender.com']
+  }
 })
